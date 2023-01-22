cdbecc9b296d8e799b8e6fa1d1030323cea0710b
Dev_Pixelabs <ruben.oliver@pixelabs.es>
2023-01-22 13:49:19 +0100

    Corregidos errores dataset y deteccion

--- datasets/dataset.py
+++ datasets/dataset.py
@@ -8,6 +8,21 @@ class VirtualKitty():
     def __init__(self, data_dir, batch_size=4):
         self.data_dir = data_dir
         self.batch_size = batch_size
+        self.classes = [[210,0,200], #Terrain
+                        [90,200,255], #Sky
+                        [0,199,0], #Tree
+                        [90,240,0], #Vegetation
+                        [140,140,140], #Building
+                        [100,60,100], #Road
+                        [250,100,255], #GuardRail
+                        [255,255,0], #TrafficSign
+                        [200,200,0], #TrafficLight
+                        [255,130,0], #Pole
+                        [80,80,80], #Misc
+                        [160,60,60], #Truck
+                        [255,127,80], #Car
+                        [0,139,139], #Van
+                        [0,0,0]] # Undefined
 
     def norm(self, image):
         ''' Normalize Image '''
@@ -33,6 +48,7 @@ class VirtualKitty():
             sample_dir = os.path.join(base_dir,"images") + os.path.sep + sample
             sample_name, _ = os.path.splitext(sample)
             sample_name_split = sample_name.split("_")
+            print("Sample load train", sample_dir   )
             image = cv.imread(sample_dir)
             image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
             image = np.pad(image, ((0,9),(0,6),(0,0)), mode='constant', constant_values=0)
@@ -66,19 +82,21 @@ class VirtualKitty():
             if i > 0 and (i%batch_size) == 0:
                 yield batch, targets
 
-    def load_test(self, shuffle=False):
+    def load_test(self):
         batch_size = self.batch_size
         # Init
         batch = np.zeros((batch_size, 3, 384, 1248))
-        targets = np.zeros((batch_size, 15, 384, 1248))
         base_dir = self.data_dir
         print("Loading dataset in: ", self.data_dir)
-        samples = os.listdir(os.path.join(base_dir,"images"))
-        if shuffle: random.shuffle(samples)
+        samples = os.listdir(base_dir)
+        print("samples", samples)
+        samples = sorted(samples)
+        # if shuffle: random.shuffle(samples)
         # Yield samples when batch is full
         i = 0
         for sample in samples:
-            sample_dir = os.path.join(base_dir,"images") + os.path.sep + sample
+            sample_dir = base_dir + os.path.sep + sample
+            print("Imagen", sample_dir)
             image = cv.imread(sample_dir)
             image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
             image = np.pad(image, ((0,9),(0,6),(0,0)), mode='constant', constant_values=0)
@@ -89,7 +107,7 @@ class VirtualKitty():
             # Yield when batch is full
             i += 1
             if i > 0 and (i%batch_size) == 0:
-                yield batch
+                yield batch.copy()
 
     def _get_image_seg_channels(self, image_ori):
         classes = [[210,0,200], #Terrain
@@ -111,6 +129,39 @@ class VirtualKitty():
         for index_class, class_seg in enumerate(classes,start=0):
             pixel_seg = np.zeros(len(classes))
             pixel_seg[index_class] = 1
-            chequeo = np.where(image_ori[:,:]==class_seg)
+            # chequeo = np.where(image_ori[:,:]==class_seg)
+            chequeo = np.where(np.all(image_ori==class_seg, axis=-1))
             image_seg[chequeo[0][:], chequeo[1][:]] = pixel_seg.astype(np.int8)
         return image_seg
+    
+    def convert_channels_toRGB(self, image):
+        image_RGB = np.zeros((384, 1248,3))
+        classes = [[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0], #Terrain
+                   [0,1,0,0,0,0,0,0,0,0,0,0,0,0,0], #Sky
+                   [0,0,1,0,0,0,0,0,0,0,0,0,0,0,0], #Tree
+                   [0,0,0,1,0,0,0,0,0,0,0,0,0,0,0], #Vegetation
+                   [0,0,0,0,1,0,0,0,0,0,0,0,0,0,0], #Building
+                   [0,0,0,0,0,1,0,0,0,0,0,0,0,0,0], #Road
+                   [0,0,0,0,0,0,1,0,0,0,0,0,0,0,0], #GuardRail
+                   [0,0,0,0,0,0,0,1,0,0,0,0,0,0,0], #TrafficSign
+                   [0,0,0,0,0,0,0,0,1,0,0,0,0,0,0], #TrafficLight
+                   [0,0,0,0,0,0,0,0,0,1,0,0,0,0,0], #Pole
+                   [0,0,0,0,0,0,0,0,0,0,1,0,0,0,0], #Misc
+                   [0,0,0,0,0,0,0,0,0,0,0,1,0,0,0], #Truck
+                   [0,0,0,0,0,0,0,0,0,0,0,0,1,0,0], #Car
+                   [0,0,0,0,0,0,0,0,0,0,0,0,0,1,0], #Van
+                   [0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]] # Undefined
+        for index_class, output_class in enumerate(classes, start=0):
+            # print("output clases", len(output_class), output_class)
+            # print("Pixel image", image[250,200], image[:,:].shape)
+            chequeo = np.where(np.all(image==output_class, axis=-1))
+            # print("chequeo", len(chequeo))
+            if len(chequeo[0]) == 0:
+                continue
+            # if len(chequeo[0]) != 0 and index_class != 14:
+                # print("clase distinta undefined", output_class, chequeo)
+            image_RGB[chequeo[0][:], chequeo[1][:]] = self.classes[index_class]
+            # print("Max image ", image_RGB.max())
+        return image_RGB
+
+        
---
+++ detect.py
@@ -0,0 +1,87 @@
+import argparse
+import torch
+import matplotlib.pyplot as plt
+from UNet.UNed_model import FCN
+import os
+import numpy as np
+import cv2 as cv
+from datasets.dataset import VirtualKitty
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-source', '--source', type=str, default="", required=True, help='Path to dataset images')
+    # parser.add_argument('-output', '--output', type=str, default="", required=True, help='Path to save inference images')
+    parser.add_argument('-device', '--device', type=int, default=0, required=True, help='Device GPU to execute')
+    parser.add_argument('-model', '--model', type=str, default="", required=True, help='Path to model')
+    parser.add_argument('-batch_size', '--batch_size', type=int, default=1, required=True, help='Batch size to train')
+    args = parser.parse_args()
+    args_parsed['source'] = args.source
+    # args_parsed['output'] = args.output
+    args_parsed['device'] = args.device
+    args_parsed['model'] = args.model
+    args_parsed['batch_size'] = args.batch_size
+    print("Args parsed ", args_parsed)
+
+if __name__ == "__main__":
+    global args_parsed
+    args_parsed = dict()
+    parse_args()
+    in_channels = 3
+    out_channels = 15
+    model = FCN(in_channels, out_channels)
+    model.load_state_dict(torch.load(args_parsed['model']))
+    model.eval()
+    model.cuda()
+    if not os.path.exists("./runs/"):
+        os.mkdir("./runs/")
+    if not os.path.exists("./runs/detect/"):
+        os.mkdir("./runs/detect/")
+    detect_runs = len(os.listdir("./runs/detect/"))
+    os.mkdir("./runs/detect/run"+str(detect_runs+1))
+    os.mkdir("./runs/detect/run"+str(detect_runs+1) + "/output/")
+    args_parsed['output'] = "./runs/detect/run"+str(detect_runs+1) + os.path.sep
+    n = (40 // args_parsed['batch_size']) # Plot about 40 samples
+    cm = "gray" # Color Map
+    virtual_kitty = VirtualKitty(args_parsed['source'], args_parsed['batch_size'])
+    batch_it = virtual_kitty.load_test()
+    for i in range(n):
+        # print("batch nexxt ", next(batch_it).shape)
+        print(f"SAMPLE #{i}")
+        batch_has_next = True
+        while batch_has_next:
+            try:
+                batch = next(batch_it)
+            # for batch in next(batch_it):
+                batch = torch.tensor(batch)
+                # Copy to GPU
+                batch = batch.float().cuda()
+                # targets = targets_one_hot.float().cuda()
+                # Forward Propagation
+                with torch.no_grad():
+                    outputs = model(batch)
+                # Plot Outputs
+                
+                for j in range(args_parsed['batch_size']):
+                    # Plot these results
+                    fig, ax = plt.subplots(nrows=3, ncols=5, figsize=(32, 32))
+                    image = np.zeros((384, 1248,15), dtype=np.int8)
+                    for channel in range(15):
+                        predictions = outputs[j,channel].cpu()
+                        mapped_predictions = predictions > 0.75
+                        image[:,:,channel] = mapped_predictions
+                        ax[channel%3][channel%5].imshow(mapped_predictions, cmap=cm)
+                        ax[channel%3][channel%5].set_title("image")
+                    image_rgb = virtual_kitty.convert_channels_toRGB(image)
+                    # OpenCV needs BGR
+                    image_BGR = cv.cvtColor(np.float32(image_rgb), cv.COLOR_RGB2BGR)
+                    cv.imwrite(args_parsed['output'] + "output" + os.path.sep + "SAMPLE_" + str(i) + "_BATCH_SAMPLE_" + str(j) + ".jpg", image_BGR)
+                    #plt.show()
+                # Clear Cache
+                del batch
+                # del targets
+                del outputs
+                torch.cuda.empty_cache()
+                break # Only process one batch for every 'i'
+            except StopIteration as e:
+                batch_has_next = False
+                print("There are not more batches")
+                continue
